83d26fda8f51c111e3c3ceccffb76baeba5e413d
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-10-31 13:37:37 +0100

    add npm package and base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/htmlcss-svelte/",
   plugins: [svelte({ experimental: { inspector: true } })]
 });
